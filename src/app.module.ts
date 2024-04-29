66453e5f2b1d0b327f1f345fc14e6c30a7cd28ea
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-04-29 11:25:20 +0200

    Fixed faulty connection with MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BeehivesController } from './beehives/beehives.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      host: process.env.MONGO_HOST,
-      port: parseInt(process.env.MONGO_PORT as string),
-      username: process.env.MONGO_USERNAME,
-      password: process.env.MONGO_PASSWORD,
-      database: process.env.MONGO_DATABASE,
+      url: process.env.MONGO_URL,
       ssl: true,
       autoLoadEntities: true,
       synchronize: process.env.NODE_ENV === 'production' ? false : true,
+      useNewUrlParser: true,
+      authSource: 'admin',
+      logging: true,
     }),
   ],
   controllers: [AppController, BeehivesController],
